b5bc803ae17359ee912ccb5f53bdcf192e6f28fb
bluerat1976 <osoblivaia76@mail.ru>
2019-07-18 17:25:59 +0300

    in process

---
+++ js/headerData.js
@@ -0,0 +1,45 @@
+export default {
+    navItems: [
+      {
+        href: 'index.html',
+        label: 'Home',
+        activeLocation: '/index.html',
+      },
+      {
+        href: 'travels.html',
+        label: 'Travels',
+        activeLocation: '/travels.html',
+      },
+      {
+        href: 'organisation.html',
+        label: 'Organisation',
+        activeLocation: '/organisation.html',
+      },
+      {
+        href: 'routes.html',
+        label: 'Routes',
+        activeLocation: '/routes.html',
+      },
+      {
+        href: 'info.html',
+        label: 'Info',
+        activeLocation: '/info.html',
+      },
+      {
+        href: 'blog.html',
+        label: 'Blog',
+        activeLocation: '/blog.html',
+      },
+      {
+        href: 'products.html',
+        label: 'Products',
+        activeLocation: '/products.html',
+      },
+      {
+        href: 'contact-us.html',
+        label: 'Contact Us',
+        activeLocation: '/contact-us.html',
+      },
+    ],
+  };
+  
