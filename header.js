e652295e4d2569dfa0204d1875e1bb0ad874e14e
bluerat1976 <osoblivaia76@mail.ru>
2019-07-19 17:28:33 +0300

    In prrocess

--- header.js
+++ header.js
@@ -1,8 +1,12 @@
+debugger;
+
 import headerTemplate from './header.hbs';
 import header from './headerData';
 import prepareHeader from './headerHelpers';
 
-var headerElement = document.getElementById('header');
+var headerElement = document.getElementById('header').innerHTML;
+Handlebars.compile(headerElement);
+
 
 if (headerElement) {
   headerElement.innerHTML = headerTemplate({ header: prepareHeader(header) });
--- headerData.js
+++ headerData.js
@@ -11,16 +11,6 @@ export default {
         activeLocation: '/travels.html',
       },
       {
-        href: 'organisation.html',
-        label: 'Organisation',
-        activeLocation: '/organisation.html',
-      },
-      {
-        href: 'routes.html',
-        label: 'Routes',
-        activeLocation: '/routes.html',
-      },
-      {
         href: 'info.html',
         label: 'Info',
         activeLocation: '/info.html',
@@ -30,16 +20,11 @@ export default {
         label: 'Blog',
         activeLocation: '/blog.html',
       },
-      {
-        href: 'products.html',
-        label: 'Products',
-        activeLocation: '/products.html',
-      },
+      
       {
         href: 'contact-us.html',
         label: 'Contact Us',
         activeLocation: '/contact-us.html',
       },
     ],
-  };
-  
+  }
