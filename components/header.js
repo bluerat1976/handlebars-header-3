a0984da97e274fae132b9e97e5460fe60c12ea18
bluerat1976 <osoblivaia76@mail.ru>
2019-07-23 15:22:03 +0300

    not working separate handlebars template

--- components/header.js
+++ components/header.js
@@ -1,12 +1,8 @@
-debugger;
-
-import headerTemplate from './header.hbs';
+import headerTemplate from '../../templates/header.hbs';
 import header from './headerData';
 import prepareHeader from './headerHelpers';
 
-var headerElement = document.getElementById('header').innerHTML;
-Handlebars.compile(headerElement);
-
+var headerElement = document.getElementById('header');
 
 if (headerElement) {
   headerElement.innerHTML = headerTemplate({ header: prepareHeader(header) });
--- healpers/ifeq.js
+++
@@ -1,5 +0,0 @@
-module.exports = function ifeq(a, b) {
-  /* eslint-disable eqeqeq */
-  return a == b;
-  /* eslint-enable eqeqeq */
-};
